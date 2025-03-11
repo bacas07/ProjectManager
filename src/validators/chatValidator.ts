d71ff083e1aedc67722f07c6a5e86ac1bf4f0949
Mateo Baca <mateobacas07@gmail.com>
2025-03-10 22:12:59 -0500

    feat: chatInputSchema method for chat validation with zod codded in chatValidator.ts

--- src/validators/chatValidator.ts
+++ src/validators/chatValidator.ts
@@ -1,5 +1,31 @@
 import { z } from 'zod';
 
+const messagesJSONSchema = z.object({
+    sender: z.string().min(1, 'Sender message is required').max(100),
+    content: z.string().min(1, 'Content message is required').max(2000)
+});
+
+const stagesContentJSONSchema = z.object({
+    name: z.string().min(1, 'Name stage is required').max(100),
+    description: z.string().min(1, 'Description stage is required').max(1000),
+    estimatedTime: z.number().positive(),
+    status: z.enum(['pending', 'in-progress', 'done'])
+});
+
+const technologiesJSONSchema = z.object({
+    name: z.string().min(1, 'technologies context is required').max(100),
+});
+
+const contextJSONSchema = z.object({
+    projectOverview: z.string().min(1, 'ProjectOverview context is required').max(1000),
+    stages: z.array(stagesContentJSONSchema),
+    technologies: z.array(technologiesJSONSchema),
+    lastUpdated: z.union([z.date(), z.string().datetime()]),
+    conversationSummary: z.string().min(1, 'ConversationSumary context is required').max(1500)
+});
+
 export const chatInputSchema = z.object({
-    
+    project: z.string().min(1, 'Project reference ID chat is required').max(100),
+    messages: z.array(messagesJSONSchema),
+    context: contextJSONSchema
 });
--- src/validators/projectValidator.ts
+++ src/validators/projectValidator.ts
@@ -3,18 +3,15 @@ import { z } from "zod";
 const stagesJSONSchema = z.object({
     name: z.string().min(1, 'Name stage is required').max(100),
     description: z.string().min(1, 'Description stage is required').max(1000),
-    stimatedTime: z.number().positive(),
-    status: z.enum([
-        'pending',
-        'in-progress',
-        'done'
-    ])
+    estimatedTime: z.number().positive(),
+    status: z.enum(['pending', 'in-progress', 'done'])
 });
+
 
 export const projectInputSchema = z.object({
     name: z.string().min(1, 'Name project is required').max(100),
     description: z.string().min(1, 'Description project is required').max(2000),
-    stack: z.string().min(1, 'Stack project is required').max(50),
+    technologies: z.string().min(1, 'technologies project is required').max(50),
     stages: z.array(stagesJSONSchema),
     createdBy: z.string().min(1, 'User ID project is required').max(100),
     deadline: z.union([z.date(), z.string().datetime()])
