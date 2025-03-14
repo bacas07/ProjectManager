61cdb6436e6e520a6687aebd1cd21af5ac676bc1
Mateo Baca <mateobacas07@gmail.com>
2025-03-14 10:20:01 -0500

    feat: added ProjectSchema for model creation in projectSchema.ts

--- src/schemas/projectSchema.ts
+++ src/schemas/projectSchema.ts
@@ -1,7 +1,80 @@
 import { Schema, Document, model } from "mongoose";
 
-interface IProject extends Document {
-    name: string,
-    description: string,
-    technologies: 
+interface IStage {
+    name: string;
+    description: string;
+    estimatedTime: number;
+    status: "pending" | "in-progress" | "done";
 }
+
+interface IProject extends Document {
+    name: string;
+    description: string;
+    technologies: string[];
+    stages: IStage[];
+    createdBy: string;
+    deadline: Date;
+}
+
+const stagesSchema = new Schema<IStage>({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 2000
+    },
+    estimatedTime: {
+        type: Number,
+        required: true
+    },
+    status: { 
+        type: String, 
+        required: true, 
+        enum: ["pending", "in-progress", "done"]
+    }
+});
+
+const projectSchema = new Schema<IProject>({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 2000
+    },
+    technologies: {
+        type: [String],
+        required: true
+    },
+    stages: {
+        type: [stagesSchema],
+        required: true
+    },
+    createdBy: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50
+    },
+    deadline: {
+        type: Date,
+        required: true
+    }
+},
+{
+    timestamps: true
+});
+
+const Project = model<IProject>("Project", projectSchema);
+export default Project
