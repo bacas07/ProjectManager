1625629e318d91e6e6ad63fc4032b40dcddb9d46
Mateo Baca <mateobacas07@gmail.com>
2025-03-14 10:22:52 -0500

    feat: Field validation and improved variable typing in UserSchema.ts

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,43 +1,43 @@
 import { Schema, Document, model } from "mongoose";
 
-interface IUSer extends Document {
-    username: string,
-    email: string,
-    password: string,
-    role: 'admin' | 'user' | 'dev',
-};
+interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    role: "admin" | "user" | "dev";
+}
 
-const userSchema = new Schema<IUSer>({
+const userSchema = new Schema<IUser>({
     username: {
         type: String,
         required: true,
+        unique: true,
         minlength: 1,
         maxlength: 50
     },
     email: {
         type: String,
         required: true,
+        unique: true,
         minlength: 1,
-        maxlength: 50
+        maxlength: 50,
+        match: /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/ // No se como funciona JAJAJA
     },
     password: {
         type: String,
         required: true,
-        minlength: 1,
+        minlength: 6,
         maxlength: 150
     },
     role: {
         type: String,
-        enum: [
-            'admin',
-            'user',
-            'dev'
-        ]
+        required: true,
+        enum: ["admin", "user", "dev"]
     }
 }, {
     timestamps: true
 });
 
-const User = model<IUSer>('User', userSchema);
+const User = model<IUser>("User", userSchema);
 
 export default User;
