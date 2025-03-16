62747df0437a061784f7024badfcb64578e00fb2
Mateo Baca <mateobacas07@gmail.com>
2025-03-16 11:46:11 -0500

    feat: added function findAll for UserModel in User.ts

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import User, { IUser } from "../schemas/userSchema";
+
+class UserModel {
+    async findAll (): Promise<IUser[] | null> {
+        try {
+            return await User.find();
+        } catch (error) {
+            console.error('Error User findd: ', error);
+            return null;
+        }
+    }
+
+    /*async findByID (user_id: string): Promise<IUser[] | null> {
+        try {
+            return await User.findById(user_id);
+        } catch (error) {
+            console.error('Error User findByID: ', error);
+            return null;
+        }
+    }*/
+}
+
+export default new UserModel();
