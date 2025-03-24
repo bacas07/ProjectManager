e471e2eee30b908cfcc7b304bfd09a5d248b896f
Mateo Baca <mateobacas07@gmail.com>
2025-03-24 16:41:39 -0500

    feat: added function create for ChatModel in Chat.ts

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -6,7 +6,47 @@ class ChatModel {
     try {
       return await Chat.find();
     } catch (error) {
-      console.error();
+      console.error('Error Chat find: ', error);
+      return null;
+    }
+  }
+
+  async findByID(chatID: String): Promise<IChat[] | null> {
+    try {
+      return await Chat.findById(chatID);
+    } catch (error) {
+      console.error('Error Chat findOne: ', error);
+      return null;
+    }
+  }
+
+  async findByProjectID(
+    projectID: FilterQuery<IChat>
+  ): Promise<IChat[] | null> {
+    try {
+      return await Chat.findOne(projectID);
+    } catch (error) {
+      console.error('Error Chat findByProjectID: ', error);
+      return null;
+    }
+  }
+
+  async findOne(filter: FilterQuery<IChat>): Promise<IChat[] | null> {
+    try {
+      return await Chat.findOne(filter);
+    } catch (error) {
+      console.error('Error Chat findOne', error);
+      return null;
+    }
+  }
+
+  async create(data: Partial<IChat>): Promise<IChat | null> {
+    try {
+      const newChat = new Chat(data);
+      return await newChat.save();
+    } catch (error) {
+      console.error('Error Chat create: ', error);
+      return null;
     }
   }
 }
