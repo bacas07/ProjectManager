5ee36b38f0932407f6d46edba1f17b1c06f38fb4
Mateo Baca <mateobacas07@gmail.com>
2025-04-04 08:04:48 -0500

    feat: added function getUserByID for UserController in userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -29,3 +29,23 @@ export const getAllUnactiveUser = async (
     next(error);
   }
 };
+
+// Funcion para obtner un usuario por su id
+export const getUserByID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByID(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
