4386d382969e4c1ebd01e1c948e79671a1dfd3e4
Mateo Baca <mateobacas07@gmail.com>
2025-04-04 08:30:34 -0500

    feat: added function getUserByEmail for UserController in userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -89,3 +89,23 @@ export const getUserByUsername = async (
     next(error);
   }
 };
+
+// Funcion para obtener un usuario por su email
+export const getUserByEmail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
