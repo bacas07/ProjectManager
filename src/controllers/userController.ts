d2c51ad5ec92b8ed041460f516f70cb897658f92
Mateo Baca <mateobacas07@gmail.com>
2025-04-04 08:15:56 -0500

    feat: added function getOneUser for UserController in userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -30,7 +30,7 @@ export const getAllUnactiveUser = async (
   }
 };
 
-// Funcion para obtner un usuario por su id
+// Funcion para obtener un usuario por su id
 export const getUserByID = async (
   req: Request,
   res: Response,
@@ -39,6 +39,26 @@ export const getUserByID = async (
   try {
     const { id } = req.params;
     const user = await User.findByID(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Funcion para obtener un usuario por un campo expecifico
+export const getOneUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const filter = req.body;
+    const user = await User.findOne(filter);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
