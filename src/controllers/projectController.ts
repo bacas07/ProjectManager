89d74d909d73dce9df69092df98352b17cca87ab
Mateo Baca <mateobacas07@gmail.com>
2025-04-04 22:43:37 -0500

    feat: added function findAllUnactiveProject for projectController in projectController.ts

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -11,11 +11,28 @@ export const getAllProject = async (
 ) => {
   try {
     const allProjects = await Project.findAll();
+    return res.status(200).json({
+      message: `Query succesful: ${allProjects?.length} project(s) found`,
+      projects: allProjects,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Funcion para obtener todos los proyectos inactivos
+export const getAllUnactiveProject = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const allUnactiveProjects = await Project.findAllUnactive();
     return res
       .status(200)
       .json({
-        message: `Query succesful: ${allProjects?.length} project(s) found`,
-        projects: allProjects,
+        message: `Query sucessful: ${allUnactiveProjects?.length} unactive project(s) found`,
+        projects: allUnactiveProjects,
       });
   } catch (error) {
     next(error);
