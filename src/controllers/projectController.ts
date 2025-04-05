da62242a64bff110b4535e12c15c1a81eb2c69f6
Mateo Baca <mateobacas07@gmail.com>
2025-04-04 22:32:05 -0500

    feat: added function findAll for projectController in projectController.ts

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,5 +1,23 @@
-import { Request, Response, NextFunction } from 'express'
-import Project from '../models/Project.js'
-import { projectInputSchema } from '../validators/projectValidator.js'
-import { parse } from 'valibot'
+import { Request, Response, NextFunction } from 'express';
+import Project from '../models/Project.js';
+import { projectInputSchema } from '../validators/projectValidator.js';
+import { parse } from 'valibot';
 
+// Funcion para obtener todos los proyecto
+export const getAllProject = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const allProjects = await Project.findAll();
+    return res
+      .status(200)
+      .json({
+        message: `Query succesful: ${allProjects?.length} project(s) found`,
+        projects: allProjects,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
